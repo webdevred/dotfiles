9c7dfda6ba502df36ac222dd76baa9d97002feaa
webdevred <webdevred>
2024-03-16 11:19:40 +0100

    initial code

---
+++ xmonad.hs
@@ -0,0 +1,27 @@
+-- a simple configuration
+
+import XMonad
+import XMonad.Util.EZConfig
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+
+import XMonad.Hooks.DynamicLog
+
+main = xmonad =<< xmobar myConfig
+
+myConfig :: XConfig (Choose Tall (Choose (Mirror Tall) Full))
+myConfig = def
+       { borderWidth        = 7
+       , terminal = "xterm"
+       , modMask = mod4Mask
+       , normalBorderColor  = "#ffaaff"
+       , focusedBorderColor = "#ffeeff"
+       , focusFollowsMouse = False}
+     `additionalKeysP`
+     [ ("M-f", spawn "firefox"  ),
+       ("M-a", spawn "emacs-gtk"),
+       ("M-d", spawn "discord"),
+       ("M-s", spawn "flameshot launcher")]
