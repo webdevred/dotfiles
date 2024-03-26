0a7339c0e8867e90021e9d9bb41ad7c3a6784b76
webdevred <webdevred>
2024-03-26 13:52:06 +0100

    add spacing

--- xmonad.hs
+++ xmonad.hs
@@ -23,6 +23,7 @@ import XMonad.Util.Loggers
 import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.Hidden
 import XMonad.Layout.Named
+import XMonad.Layout.Spacing
 
 import System.IO (hPutStrLn)
 import Data.Char (toLower)
@@ -104,11 +105,10 @@ myConfig configLocation =
          ("M-c", spawn "caja"),
          ("M-x", spawn "rofi -show drun -config ~/.xmonad/rofi/config.rasi"),
          ("M-z", spawn "rofi -show window -config ~/.xmonad/rofi/config.rasi"),
-         ("M-f", sendMessage $ JumpToLayout "Full"),
+         ("M-S-f", sendMessage $ JumpToLayout "Full"),
          ("M-S-b", sendMessage $ JumpToLayout "Big Master Tall"),
-         ("M-t", sendMessage $ JumpToLayout "Tall"),
-         ("M-C-<Space>", namedScratchpadAction scratchpads "emacs-scratch"),
-         ("M-C-h", spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
+         ("M-S-t", sendMessage $ JumpToLayout "Tall"),
+         ("M-C-<Space>", namedScratchpadAction scratchpads "emacs-scratch")
         ]
 
 myManageHook :: ManageHook
@@ -125,62 +125,10 @@ scratchpads = [NS "emacs-scratch" spawnEmacsScratch findEmacsScratch manageEmacs
         manageEmacsScratch = nonFloating
 
 myLayouts =
-  (Tall 1 (1/2) (1/2))
-  ||| named "Big Master Tall" (Tall 1 (1/2) (2/3))
+  named "Tall" (spacing 10 $ Tall 1 (1/2) (1/2))
+  ||| named "Big Master Tall" (spacing 10 $ Tall 1 (1/2) (2/3))
   ||| Full
   
 
 myStartup = do
   spawnOnce "feh --bg-fill /mnt/HDD/bakgrund2.jpg /mnt/HDD/bakgrund.jpg"
-
-
--- | Finally, a copy of the default bindings in simple textual tabular format.
-help :: String
-help = unlines ["The default modifier key is 'alt'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Shift-Enter  Launch xterminal",
-    "mod-p            Launch dmenu",
-    "mod-Shift-p      Launch gmrun",
-    "mod-Shift-c      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "",
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "",
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "mod-[1..9]   Switch to workSpace N",
-    "",
-    "-- Workspaces & screens",
-    "mod-Shift-[1..9]   Move client to workspace N",
-    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
-    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
-    "",
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging"]
