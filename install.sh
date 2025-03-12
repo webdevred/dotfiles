55116c3ec274a6f5470fc7d41e67d7ede22f6ebc
webdevred <webdevred>
2025-03-12 14:57:40 +0100

    improve install script

--- install.sh
+++ install.sh
@@ -1,13 +1,66 @@
 #!/bin/sh
 
-for util in $(cat dest_places.txt); do        
-    SRC=$(echo -n "$util" | awk -F ':' '{ print $1}')
-    DST=$(echo -n "$util" | awk -F ':' '{ print $2}')
-    if echo -n "$1" | grep -q 'u'; then
-        rm -rf $(dirname "$HOME/$DST")
+dottfiles_link() {
+  config_dst_dir="$(dirname "$2")"
+
+  if ! [ -d "$config_dst_dir" ]; then
+    set -x
+    mkdir -p "$config_dst_dir"
+    { set +x; } 2>/dev/null
+  fi
+
+  if ! [ -e "$2" ]; then
+      set -x
+      ln -sf "$1" "$2"
+      { set +x; } 2>/dev/null
+  fi
+}
+
+dotfiles_remove() {
+  if ! [ -e $1 ]; then
+    return
+  fi
+  set -x
+  rm -rf "$1"
+  { set +x; } 2>/dev/null
+}
+
+if echo -n "$1" | grep -Eq '^-?i$' && [ -n "$2" ]; then
+    action=1
+    util_to_install=$2
+elif echo -n "$1" | grep -Eq '^-?u$'; then
+  action=2
+  util_to_remove="$2"
+else
+    action=3
+fi
+
+for util in $(cat dest_places.txt | sed -E '/^(#.*)?$/d'); do
+  dot_file=$(echo -n "$util" | awk -F ':' '{ print $1}')
+  config_dst=$(echo -n "$util" | awk -F ':' '{ print $2}')
+
+  if [ -z "$dot_file" ] || [ -z "$config_dst" ]; then
+    echo "invalid line $util"
+    continue
+  fi
+
+  if [ $action -eq 3 ] || { [ $action -eq 1 ] && echo -n "$dot_file" | grep -Eq '^'"$util_to_install"''; }; then
+    if [ -d "$dot_file" ]; then
+      for file in $(find "$dot_file" -type f -not -name '.*' -not -name '*~'); do
+        single_filename=$(realpath --relative-to="$dot_file" "$file")
+        single_dst_path="$HOME/$config_dst/$single_filename"
+        dottfiles_link "$(realpath $file)" "$single_dst_path"
+      done
     else
-        echo $SRC $HOME/$DST
-        mkdir -p $(dirname "$HOME/$DST")
-        ln -sf $(realpath $SRC) "$HOME/$DST"
+      real_dotfile_path=$(realpath "$dot_file")
+      dottfiles_link "$real_dotfile_path" "$HOME/$config_dst"
     fi
+  elif [ $action -eq 2 ] && { [ -z "$util_to_remove" ] || echo -n "$dot_file" | grep -Eq '^'"$util_to_remove"''; }; then
+    config_dst_base_dir=$(echo -n "$config_dst" | grep -oh '^.config/[^/]*')
+    if [ -n "$config_dst_base_dir" ] && [ -d "$HOME/$config_dst_base_dir" ]; then
+      dotfiles_remove "$HOME/$config_dst_base_dir"
+    else
+      dotfiles_remove "$HOME/$config_dst"
+    fi
+  fi
 done
