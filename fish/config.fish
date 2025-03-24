251f4cad89e835ad1d2e9de42228534f254d4f61
webdevred <webdevred>
2025-03-24 23:22:18 +0100

    add vimrc

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,10 @@ set -g fish_greeting
 set -x EDITOR vim
 set -x SYSTEMD_EDITOR vim
 
+set -x vi
+
+set -U fish_user_paths ~/.local/bin $fish_user_paths
+
 if status is-interactive
   # Commands to run in interactive sessions can go here
 end
