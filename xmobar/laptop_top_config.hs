14515688c5e0ca8349475c5b69a3e4599391b3c2
webdevred <148627186+webdevred@users.noreply.github.com>
2025-04-18 21:18:44 +0200

    add panel with battery for laptop

---
+++ xmobar/laptop_top_config.hs
@@ -0,0 +1,27 @@
+Config { font = "DejaVu Sans Mono 10"
+       , borderColor = "black"
+       , border = FullB
+       , bgColor = "black"
+       , fgColor = "#ffaaff"
+       , alpha = 128
+       , position = Top
+       , textOffset = -1
+       , iconOffset = -1
+       , lowerOnStart = True
+       , pickBroadest = False
+       , persistent = False
+       , hideOnStart = False
+       , iconRoot = "."
+       , allDesktops = True
+       , overrideRedirect = True
+       , textOutputFormat = Ansi
+       , commands = [ Run Cpu ["-L","3","-H","50", "--normal","#ffaaff","--high","red"] 10
+                    , Run Memory ["-t","Mem: <usedratio>%"] 10
+                    , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
+                    , Run Com "python3" [".config/xmobar/pulseaudio_status.py"] "pulseaudio" 60
+                    , Run Battery [ "-t", "<left>% / <timeleft>" ] 60
+                    ]
+       , sepChar = "%"
+       , alignSep = "}{"
+       , template = "}{ %pulseaudio% | %battery% | %memory% | %cpu% | %date%"
+       }
