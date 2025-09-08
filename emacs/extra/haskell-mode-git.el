0e4ef6f646a83d94b26078ec677bdd463778c51a
debbie <148627186+webdevred@users.noreply.github.com>
2025-09-08 06:56:47 +0200

    parse the cabal file to set cabal repl args

--- emacs/extra/haskell-mode-git.el
+++ emacs/extra/haskell-mode-git.el
@@ -32,6 +32,81 @@
 
 (add-hook 'haskell-mode-hook #'my-haskell-mode-setup)
 
+
+;; ----------------
+;; parse the cabal file to set cabal repl args in case .dir-locals.el wasnt found
+;; ----------------
+(defun cabal--parse-components (cabal-file)
+  "Return an alist of (COMPONENT . SOURCE-DIRS) from CABAL-FILE.
+COMPONENT is a string like 'library' or 'executable foo'."
+  (with-temp-buffer
+    (insert-file-contents cabal-file)
+    (goto-char (point-min))
+    (let (result current-comp)
+      (while (not (eobp))
+        (when (looking-at "^\\(library\\|executable\\|test-suite\\)\\(?:\\s-+\\([^ \n]+\\)\\)?")
+          (setq current-comp
+                (if (match-string 2)
+                    (cons (match-string 1) (match-string 2))
+                  (match-string 1))))
+        (when (and current-comp
+                   (looking-at "^\\s-+hs-source-dirs:\\s-*\\(.*\\)"))
+          (let ((dirs (split-string (match-string 1) "[ \t]+" t)))
+            (push (cons current-comp dirs) result)))
+        (forward-line 1))
+      (nreverse result))))
+
+(defun cabal--get-project-name ()
+  "Return the `name` field from the `.cabal` file of the current project."
+  (let* ((project-root (projectile-project-root))
+         (cabal-file (car (directory-files project-root t "\\.cabal\\'"))))
+    (when cabal-file
+      (with-temp-buffer
+        (insert-file-contents cabal-file)
+        (goto-char (point-min))
+        (when (re-search-forward "^name:\\s-*\\(.*\\)$" nil t)
+          (string-trim (match-string 1)))))))
+
+(defun get-cabal-target-for-buffer ()
+  "Return the Cabal component of the current buffer (library, executable:foo, etc.)."
+  (let* ((project-root (projectile-project-root))
+         (current-file-name (file-relative-name (buffer-file-name) project-root))
+         (cabal-file (car (directory-files project-root t "\\`[^.#][A-Za-z0-9_.-]*\.cabal\\'")))
+         (project-name (cabal--get-project-name)))
+    (when cabal-file
+      (let ((components (cabal--parse-components cabal-file))
+            (result nil))
+        (dolist (comp components)
+          (let* ((name (car comp))
+                 (dirs (cdr comp)))
+            (when (cl-some (lambda (dir)
+                             (string-prefix-p dir current-file-name))
+                           dirs)
+              (setq result
+                    (cond
+                     ((string-match-p "\\`library" (car name))
+                      (concat "lib:" project-name))
+                     ((string-match-p "\\`executable" (car name))
+                      (concat project-name ":exe:" (cdr name)))
+                     ((string-match-p "\\`test-suite" (car name))
+                      (concat project-name ":test:" (cdr name)))
+                     (t nil))))))
+        result))))
+
+(defun my-haskell-add-cabal-target-arg (orig-fun session hptype)
+  "Advice around `haskell-process-compute-process-log-and-command' to add
+an extra argument to `haskell-process-args-cabal-repl' dynamically."
+  (if (eq hptype 'cabal-repl)
+      (let* ((target (get-cabal-target-for-buffer))
+             (haskell-process-args-cabal-repl
+              (if target
+                  (cons target haskell-process-args-cabal-repl)
+                haskell-process-args-cabal-repl)))
+        (funcall orig-fun session hptype))
+    (funcall orig-fun session hptype)))
+
+(advice-add 'haskell-process-compute-process-log-and-command :around
+            #'my-haskell-add-cabal-target-arg)
 (defvar my-haskell-dir-locals-last-dir nil
   "Holds the last directory for which Haskell dir-locals were applied for the current buffer.")
 
